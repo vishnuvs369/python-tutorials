e78f78ae8811a8a8537cdb5f5b551ad3db2b92e8
vishnu vs <54799446+vishnuvs369@users.noreply.github.com>
2021-10-01 22:13:10 +0530

    added operators

---
+++ operators.py
@@ -0,0 +1,42 @@
+#Addition
+#substraction
+#multiplication
+#division
+#modulus
+
+
+a = 10
+b = 3
+
+x = a + b
+print(x)
+
+x = a - b
+print(x)
+
+x = a/b
+print(x)
+
+x = a%b
+print(x)
+
+#--------------------------------------
+
+x = a/b
+print(x)
+
+#--For avoiding decimal values
+x = a//b  
+print(x)
+
+#---------------------------------------
+
+#Exponential
+
+x = 2
+print(x ** 2)
+
+#--------------------------------------
+
+x = 10
+print(x == 10 and x < 15)
