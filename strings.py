44bafcbce5d16289f009b343745546791a99db07
vishnu vs <54799446+vishnuvs369@users.noreply.github.com>
2021-10-01 23:44:07 +0530

    added strings

---
+++ strings.py
@@ -0,0 +1,37 @@
+#text = "Learn Python"
+text = 'Learn Python'
+print(text)
+
+#---------------------------------------
+
+text = """
+Learn python
+Learn c++
+Learn java
+"""
+print(text)
+
+#--------------------------------------
+
+text = "Learn Python"
+
+print(text[0])
+print(text[6])
+
+print(text[0:4])
+ 
+print(len(text))
+
+#---------------------------------------
+
+text = "LEARN PYTHON"
+
+print(text.lower())
+
+#---------------------------------------
+
+text = "learn python"
+
+print(text.replace("p", "v"))
+
+#---------------------------------------
