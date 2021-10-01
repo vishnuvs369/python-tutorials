6c2dbeb1f38147131ccb83088a1c586ad0ca9c92
vishnu vs <54799446+vishnuvs369@users.noreply.github.com>
2021-10-01 18:14:45 +0530

    variablesanddatatypes added

---
+++ variable_datatypes.py
@@ -0,0 +1,60 @@
+# variable declaring
+
+x = 10
+print(x)
+
+name = "vishnu"
+print(name)
+
+num = 4.56
+print(num)
+
+#------------------------------------------------
+# Checking type 
+
+print(type(x))
+
+print(type(name))
+
+print(type(num))
+
+
+#-------------------------------------------------
+
+# Boolean datatype
+
+python = True
+
+java = False
+
+print(type(python))
+
+print(type(java))
+
+#----------------------------------------------
+
+# Variable assigning
+
+# x = 10
+# y = 10
+# z = 10
+
+x=y=z= 10
+
+print(x)
+print(y)
+print(z)
+
+#--------------------------------------------
+
+#x = "apple"
+#y = "orange"
+#z = "mango"
+
+x , y , z = "apple", "orange", "mango"
+
+print(x)
+print(y)
+print(z)
+
+#---------------------------------------------
