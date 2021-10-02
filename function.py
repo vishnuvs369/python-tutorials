19d6ca0239b42888a2b3a3f5300777dd9a3a5b40
vishnu vs <54799446+vishnuvs369@users.noreply.github.com>
2021-10-02 23:11:14 +0530

    added functions

---
+++ function.py
@@ -0,0 +1,38 @@
+# function
+
+def message():
+    print("Hello World")
+
+message()  
+
+#message() 
+
+#message() 
+
+#---------------------------------------------------
+
+def message(name):
+    print("hello " +name)
+
+message("vishnu")
+
+message("arun")
+
+
+#---------------------------------------------------
+
+def sum(num1, num2):
+   print(num1+num2)
+
+sum(10,20)
+
+
+#--------------------------------------------------
+
+def sum():
+    return(20+30)
+
+print(sum())
+
+
+#---------------------------------------------------
