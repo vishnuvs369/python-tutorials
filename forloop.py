378ad4844ac0e4ab693f3a2e0d51937260b2bd21
vishnu vs <54799446+vishnuvs369@users.noreply.github.com>
2021-10-02 16:57:00 +0530

    added forloop

---
+++ forloop.py
@@ -0,0 +1,31 @@
+# forloop
+
+x = "HELLO"
+
+for i in x:
+    print(i)
+
+
+
+for i in x[2]:
+    print(i)   
+
+#------------------------------------------
+
+list1 = ["orange", "apple", "mango"]
+
+for x in list1:
+    print(x)
+
+
+#------------------------------------------
+
+# print numbers form 1 to 5 using forloop
+
+for n in range(5):
+    print(n)
+
+
+# numbers from 5 to 11
+for n in range(5,11):
+    print(n)
