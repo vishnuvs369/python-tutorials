06f82e5dc6fe7136e3ccc89167ec56c0be4b6b52
vishnu vs <54799446+vishnuvs369@users.noreply.github.com>
2021-10-02 12:13:15 +0530

    added tuple

---
+++ tuple.py
@@ -0,0 +1,22 @@
+# tuple
+
+tuple1 = ("SAMSUNG", "IPHONE", "ONEPLUS")
+# del tuple1
+
+print(tuple1)
+
+#print(tuple1[0])
+
+#print(len(tuple1))
+
+
+#for X in tuple1:
+  #print(tuple1)
+
+
+#Tuples are unchangeable, meaning that we cannot change, add or remove items after the tuple has been created.
+#tuple1[0] = "REDMI"
+#print(tuple1)
+
+
+
