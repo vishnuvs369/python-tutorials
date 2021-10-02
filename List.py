1c1d5c030d608c959af12e1df3a301cb07d39855
vishnu vs <54799446+vishnuvs369@users.noreply.github.com>
2021-10-02 11:19:23 +0530

    Added List

---
+++ List.py
@@ -0,0 +1,61 @@
+# List
+
+list1 = ["apple", "orange", "mango"]
+
+print(list1)
+
+print(len(list1))
+
+print(list1[1])
+
+#------------------------------------------------
+
+list2= [10,20,30,40]
+
+print(list2)
+
+print(list2[0]+list2[1])
+
+print(list2[3]-list2[1])
+
+#-------------------------------------------------
+
+list3= [10,20,30,40]
+
+sum = list3[0] + list3[1]
+
+print(sum)
+
+#--------------------------------------------------
+
+list4 = ["BMW", "AUDI", "HONDA"]
+
+# list4[0] = "FERRARI"
+# print(list4)
+
+
+#for x in list4:
+#   print(x)
+
+
+
+#list4.append("BENZ")
+#print(list4)
+
+
+list4.insert(1, "BENZ")
+#print(list4)
+
+
+list4.remove("HONDA")
+#print(list4)
+
+
+#list4.clear()
+#print(list4)
+
+
+#del list4
+#print(list4)
+
+#----------------------------------------------
