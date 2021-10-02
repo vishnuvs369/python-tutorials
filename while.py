dcfc05d0c258507f3da79698b37aae2c3101abf5
vishnu vs <54799446+vishnuvs369@users.noreply.github.com>
2021-10-02 16:30:30 +0530

    added while loop

---
+++ while.py
@@ -0,0 +1,25 @@
+#While Loop
+
+# Print the numbers from 1 to 5 using while loop
+
+i = 1 
+
+while i < 6:
+    print(i)
+    i += 1    # i=i+1
+
+
+#-------------------------------------------------
+
+#print hello user 5 times
+'''
+i = 1
+a = "Hello user"
+
+while i < 6:
+    print(a)
+    i += 1
+'''
+
+
+#------------------------------------------------
