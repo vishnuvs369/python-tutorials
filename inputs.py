8afc96e182ab2a78c9ed5ae652761d49e9f05069
vishnu vs <54799446+vishnuvs369@users.noreply.github.com>
2021-10-04 19:07:56 +0530

    added inputs

---
+++ inputs.py
@@ -0,0 +1,21 @@
+# inputs
+
+name = input("Enter your name")
+email = input("Enter your email")
+
+print(name , email)
+
+
+#-------------------------------------------------
+
+
+num1 = input("Enter the first number")   #The input number is in string we need to convert to integer
+num2 = input("Enter the second number")
+
+
+#num1 = int(input("Enter the first number"))  
+#num2 = int(input("Enter the second number"))
+
+sum = num1 + num2  
+
+print(sum)
